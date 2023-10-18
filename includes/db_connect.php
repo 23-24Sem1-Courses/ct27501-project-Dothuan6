ff519e7c569afee9fe277330376e82e6628a68bd
Dothuan6 <thuann6222@gmail.com>
2023-10-18 11:50:20 +0700

    Connect PDO

---
+++ includes/db_connect.php
@@ -0,0 +1,19 @@
+<?php 
+
+# server name
+$host = "localhost";
+# user name
+$username = "root";
+# password
+$password= "123456789";
+
+# database name
+$db_name = "2tstore";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", 
+                    $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  echo "Connection failed : ". $e->getMessage();
+}
