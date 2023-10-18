7f24c4e0a40fab65cd2f2f1a8ce3a42e9df531c2
Dothuan6 <thuann6222@gmail.com>
2023-10-18 13:56:12 +0700

    2Tshop

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,13 +14,94 @@
 
 <body>
     <div class="container-fluid">
-        <?php include_once('./../includes/navbar.php') ?>
+        <div class="container-fluid">
+            <?php include_once('./../includes/navbar.php') ?>
+
+        </div>
+        <!-- second child -->
+        <nav class="navbar-expand-lg navbar-dark text-dark px-3" style="display: block;">
+            <ul class="navbar-nav me-auto py-2 ">
+
+                <?php 
+  if(isset($_SESSION['username'])){
+    echo "<li class='nav-item p-0'>
+          <a class='nav-link text-dark p-1' href='index.php'>Xin chào {$_SESSION['username']} &rang;</a>
+          </li>";
+  }else{
+    echo "<li class='nav-item'>
+    <a class='nav-link text-dark' href='index.php'><i class='fa-regular fa-user'></i> </a>
+    </li>";
+  }
+  if(isset($_SESSION['username'])){
+    echo "
+    <li class='nav-item'>
+           <a class='nav-link text-dark p-1' href='./user_areas/user_logout.php'>Đăng xuất</a>
+    </li>";
+  }else{
+    echo "
+    <li class='nav-item'>
+           <a class='nav-link text-dark px-1' href='./user_areas/user_login.php'> Đăng nhập</a>
+    </li>";
+  }
+  ?>
+            </ul>
+        </nav>
+        <!--  -->
+        <div class="row">
+            <h2 class="text-center">
+                2TShop-CAR
+            </h2>
+            <!-- slider -->
+            <div>
+                <div id="carouselExampleCaptions" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
+                            class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="../images/slider1.jpg" class="d-block w-100" alt="ảnh">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../images/slider2.webp" class="d-block w-100" alt="ảnh">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../images/slide3.pngs" class="d-block w-100" alt="ảnh">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+
 
     </div>
-
-
-
-
     <?php
     include_once ('../includes/footer.php');
      ?>
