71a2e2280803b4e426791f00684a90817289e5ca
Dothuan6 <thuann6222@gmail.com>
2023-10-18 11:50:54 +0700

    index

--- public/index.php
+++ public/index.php
@@ -1,1 +1,26 @@
-<h1>heko</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Chủ</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <?php include_once('./../includes/navbar.php') ?>
+
+    </div>
+
+
+
+
+    <?php
+    include_once ('../includes/footer.php');
+     ?>
+</body>
+
+</html>
