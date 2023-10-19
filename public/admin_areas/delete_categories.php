55cee33d01367b47545bc08a6d03de00de6f014a
Dothuan6 <thuann6222@gmail.com>
2023-10-19 22:27:44 +0700

    Admin/deletecat

---
+++ public/admin_areas/delete_categories.php
@@ -0,0 +1,12 @@
+<?php
+if(isset($_GET['delete_category'])){
+    $get_category_id=$_GET['delete_category'];
+    $delete_category="delete from `categories` where category_id = ?";
+    $stmt=$conn->prepare($delete_category);
+    $result_delete_category= $stmt->execute([$get_category_id]);
+    if($result_delete_category){
+        echo "<script>alert('Xóa thành công!')</script>";
+        echo "<script>window.open('./index.php?insert_categories','_self')</script>";
+    }
+}
+ ?>
