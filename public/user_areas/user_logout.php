a4df37ef630a838acb6c980551f1436a09a7cfa1
Dothuan6 <thuann6222@gmail.com>
2023-10-21 17:06:11 +0700

    delete

--- public/user_areas/user_logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-echo "<script>alert('Bạn đã đăng xuất khỏi tài khoản này!')</script>";
-echo "<script>window.open('../index.php','_self')</script>"
-?>
