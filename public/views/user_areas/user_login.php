6f00bb210c149b816395f9df17737c1262109f1a
Dothuan6 <thuann6222@gmail.com>
2023-10-27 18:52:46 +0700

    User/login

--- public/views/user_areas/user_login.php
+++ public/views/user_areas/user_login.php
@@ -33,9 +33,16 @@ include_once __DIR__ . '/../../controllers/function.php';
                         <label for="user_password" class="form-label"><i class="fa-regular fa-eye"></i> Mật khẩu</label>
 
                     </div>
+                    <div class="form-floating mb-4">
+                        <!-- password field -->
+                        <input type="text" class="form-control" id="user_admin_code" placeholder="Nhập vào mã quản lý"
+                            autocomplete="off" name="user_admin_code">
+                        <label for="user_admin_code" class="form-label">Mã quản lý</label>
+                    </div>
                     <input class="mt-4 btn btn-info mb-6 px-3" type="submit" name="user_login" id="user_login"
                         value="Đăng nhập">
-                    <p class="small fw-bold mt-2 pt-1 mb-0">Chưa có mật khẩu?<a class="text-danger" href="user_reg.php">
+                    <p class="small fw-bold mt-2 pt-1 mb-0">Chưa có tài khoản?<a class="text-danger"
+                            href="user_reg.php">
                             Đăng ký</a></p>
                 </form>
             </div>
