680efe6de4dbd08584966610aa9cd73bc36e62d9
Dothuan6 <thuann6222@gmail.com>
2023-10-27 11:22:55 +0700

    payment

--- public/views/user_areas/payment.php
+++ public/views/user_areas/payment.php
@@ -9,13 +9,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thanh Toán</title>
-    <!-- css link bstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
 <body>
     <!--php access user id  -->
+    <?php include_once __DIR__ . '/../../../includes/header.php' ?>
     <?php
      global $con;
      if(isset($_GET['user_id'])){
@@ -27,7 +25,7 @@
         <div class="my-5 row d-flex justify-content-center align-items-center">
             <div class="col-md-6">
                 <a href="order.php?user_id=<?php echo $get_userid ?>" target="_plank">
-                    <h2 class="text-center">Thanh toán khi nhận hàng</h2>
+                    <h3 class="text-center">Thanh toán khi nhận hàng</h3>
                 </a>
             </div>
         </div>
