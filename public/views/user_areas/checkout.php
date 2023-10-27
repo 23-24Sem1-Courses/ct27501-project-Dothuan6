1d1993cc181ff640048253629f457bac537bfd19
Dothuan6 <thuann6222@gmail.com>
2023-10-27 11:22:47 +0700

    checkout

--- public/views/user_areas/checkout.php
+++ public/views/user_areas/checkout.php
@@ -10,9 +10,6 @@ include_once __DIR__ .'/../../../public/controllers/function.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chi tiết giỏ hàng</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
     <style>
     .cart_img {
         width: 100px !important;
@@ -22,15 +19,13 @@ include_once __DIR__ .'/../../../public/controllers/function.php';
 </head>
 
 <body>
-
     <div class="container-fluid">
         <div class="container-fluid">
             <!-- navbar -->
             <?php
-    include_once __DIR__ .  '/../../../includes/header.php';
-    include_once ('./includes/navbar.php');
-
-    ?>
+            include_once __DIR__ .  '/../../../includes/header.php';
+            include_once ('./includes/navbar.php');
+            ?>
             <h3 class="text-center py-3">Chi tiết giỏ hàng</h3>
             <div class="container">
                 <div class="row">
