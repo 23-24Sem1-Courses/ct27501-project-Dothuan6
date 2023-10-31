8ec67d3bf5e1da9063a557aef3bdb36249afd0ef
Dothuan6 <thuann6222@gmail.com>
2023-10-31 12:41:09 +0700

    search_pro

--- public/search_products.php
+++ public/search_products.php
@@ -10,27 +10,6 @@ include_once __DIR__ . '/controllers/function.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang Chủ</title>
     <link rel="stylesheet" href="./css/style.css">
-    <style>
-    .marquee {
-        width: 10%;
-        white-space: nowrap;
-        overflow: hidden;
-        box-sizing: border-box;
-        animation: marquee 20s linear infinite;
-        border-radius: 5px;
-        margin-bottom: 10px;
-    }
-
-    @keyframes marquee {
-        0% {
-            transform: translateX(100%);
-        }
-
-        100% {
-            transform: translateX(-100%);
-        }
-    }
-    </style>
 </head>
 
 <body>
@@ -48,10 +27,7 @@ include_once __DIR__ . '/controllers/function.php';
         </nav>
         <!--  -->
         <div class="row">
-            <div class="marquee fw-bold py-3">
-                <span><i class="fa-solid fa-tag"></i> TÌM KIẾM SẢN PHẨM!</span>
-            </div>
-            <h3 class="text-center">TẤT CẢ SẢN PHẨM</h3>
+            <h3 class="text-center">SẢN PHẨM TÌM KIẾM</h3>
         </div>
         <div class="container">
             <div class="row">
