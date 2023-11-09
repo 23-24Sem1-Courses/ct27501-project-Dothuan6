49a78a9b570afd41ae86b8106ede7abb689ce482
Dothuan6 <thuann6222@gmail.com>
2023-11-09 20:41:27 +0700

    controller/contact

--- public/controller/contact.php
+++ public/controller/contact.php
@@ -1,12 +1,8 @@
 <?php
 @session_start();
  ?>
-<div class="container-fluid fixed-top">
-    <?php include_once('./../view/header.php') ?>
-</div>
-<?php
-        include_once('./../view/contact.php'); 
-        ?>
-<?php
-    include_once __DIR__ . '/../view/footer.php';
+<?php 
+    include_once('./../view/header.php');
+    include_once('./../view/contact.php'); 
+    include_once('./../view/footer.php');
      ?>
